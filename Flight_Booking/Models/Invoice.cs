52bf3eb852516b8d73e66958759b6f43cc9c9b88
letruongluat <lluat91@gmail.com>
2025-04-05 22:36:01 +0700

    thư mục Flight_Booking

---
+++ Flight_Booking/Models/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightBookingApp.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+
+        [Required]
+        public int BookingId { get; set; }
+
+        public string? CompanyName { get; set; } // Cho phép NULL
+
+        public string? CompanyAddress { get; set; } // Cho phép NULL
+
+        public string? CompanyCity { get; set; } // Cho phép NULL
+
+        public string? TaxCode { get; set; } // Cho phép NULL
+
+        public string? InvoiceRecipient { get; set; } // Cho phép NULL
+
+        public string? InvoicePhone { get; set; } // Cho phép NULL
+
+        public string? InvoiceEmail { get; set; } // Cho phép NULL
+
+        public Booking Booking { get; set; }
+    }
+}
---
+++ Flight_Booking/Models/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightBookingApp.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+
+        [Required]
+        public int BookingId { get; set; }
+
+        [Required]
+        public string? PaymentMethod { get; set; } // Cho phép NULL
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime PaymentDate { get; set; }
+
+        [Required]
+        public string? Status { get; set; } // Cho phép NULL
+
+        public Booking Booking { get; set; }
+    }
+}

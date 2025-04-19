272dd675377c05d3637ccf3aa9f60a2b8a5371e3
letruongluat <118273085+letruongluat@users.noreply.github.com>
2025-04-19 08:30:38 +0700

    Flight_Booking directory

--- Flight_Booking/Models/WebsiteVisit.cs
+++
@@ -1,8 +0,0 @@
-﻿public class WebsiteVisit
-{
-    public int Id { get; set; }
-    public DateTime VisitDate { get; set; }
-    public int? UserId { get; set; }
-    public string IpAddress { get; set; }
-    public string Path { get; set; }
-}
